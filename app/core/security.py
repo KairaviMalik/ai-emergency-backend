7e8a1ad6dc55f5a11fcc35f83d1a13afdca91d25
Kairavi Malik <24f3000730@ds.study.iitm.ac.in>
2026-05-30 22:55:32 +0530

    fix render deployment issues

--- app/core/security.py
+++ app/core/security.py
@@ -9,9 +9,11 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def hash_password(password: str):
+    password = password[:72]  # bcrypt limit protection
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str):
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
